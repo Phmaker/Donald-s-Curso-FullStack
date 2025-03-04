464ed9aa08f4036048fe398f6d44112db2390f9e
Pedro Henrique Cadete <pedrohenriquecadetepf@gmail.com>
2025-03-04 13:53:03 -0300

    feat: add shadcn initial setup and components

---
+++ src/app/products/page.tsx
@@ -0,0 +1,11 @@
+
+const ProductPage = () => {
+    return <div className="p-5 border border-red-500 rounded-xl">
+        <h1 className="text-red-500">Product Page</h1>
+        <button>FSW 7.0</button>
+        <input placeholder="Digite Algo!"/>
+
+    </div>;
+}
+ 
+export default ProductPage;
